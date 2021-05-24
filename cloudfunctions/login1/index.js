6f43ac665b6a81add538866b3d57ea049daa3228
NierJerome <2594099901@qq.ocm>
2021-05-24 19:18:38 +0800

    登录调试

---
+++ cloudfunctions/login1/index.js
@@ -0,0 +1,74 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: 'dev-cloud-4gsfo39c022ee2d2'
+})
+
+const rp = require('request-promise');
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  let res = await db.collection('user')
+    .where({
+      openid: wxContext.OPENID
+    })
+    .get()
+  console.log(res);
+  if (!res.data.length) {
+    db.collection('user')
+      .add({
+        data: {
+          openid: wxContext.OPENID
+        }
+      })
+  }
+  // if (isNew) {
+  //   console.log("新增");
+  // }
+  // ?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+  let options = {
+    uri: 'https://api.weixin.qq.com/sns/jscode2session',
+    qs: {
+      appid: "wx3357030c8ada9441",
+      secret: "91101a9c895ceb00e6ad9b266a3e18ad",
+      js_code: event.code,
+      grant_type: "authorization_code"
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  }
+  cloud.session_key = "1"
+  
+  console.log(cloud.session_key);
+  return await rp(options)
+    .then((res) => {
+      let loginInfo = {
+        openid: wxContext.OPENID,
+        appid: wxContext.APPID,
+        // 环境id用作开发时查看云函数所在环境   
+        // -----------------------待实现
+        env: wxContext.ENV,
+        ...res
+      }
+      db.collection('user').where({
+        openid: wxContext.OPENID
+      }).update({
+        // data 传入需要局部更新的数据
+        data: {
+          session_key:loginInfo.session_key
+        },
+        success: function (res) {
+          console.log(res.data)
+        }
+      })
+      return loginInfo
+    })
+    .catch((err) => {
+      return err
+    })
+
+}
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,11 @@
 //app.js
 App({
   onLaunch: function () {
+    
+    // 匹配用户id
+    // if (condition) {
+      
+    // }
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -9,11 +14,34 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
+        env: 'dev-cloud-4gsfo39c022ee2d2',
         traceUser: true,
       })
+      console.log("[当前环境]：[dev]");
     }
 
-    this.globalData = {}
+    wx.login({
+      timeout: 30000,
+      success: (res) => {
+        // console.log(res);
+        wx.cloud.callFunction({
+          name: 'login1',
+          data: {
+            code: res.code
+          },
+          success: (res) => {
+            console.log('[云函数] [login] user openid: ', res)
+            wx.setStorageSync('loginInfo', res.result)
+          }
+        })
+      },
+      fail: (err) => {
+        console.log(err);
+      }
+    })
+
+    this.globalData = {
+      userInfo: null,
+    }
   }
 })
